ed6168641bf1089f2f79bd2be1914123c63c1b20
khaimustafa <222735@student.upm.edu.my>
2026-01-27 06:09:15 +0800

    Updated for maintanence

---
+++ Downloads/spring-petclinic-main (4)/spring-petclinic-main/README.md
@@ -0,0 +1,11 @@
+# Spring PetClinic Sample Application
+
+## Team Maintenance Project
+This project is being maintained by Group EvoTEch for SWE4351 Software Maintenance course.
+
+Team Members:
+- NUR KHAIRUNNISA BINTI MUSTAFA KAMAL ARIFIN
+- NUR KHAIRUNNISA BINTI MUSTAFA KAMAL ARIFIN
+- NURSARAH ATHIRAH BINTI ISA
+
+Semester 1, 2025/2026
---
+++ Downloads/spring-petclinic-main (4)/spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.owner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.core.style.ToStringCreator;
+import org.springframework.samples.petclinic.model.Person;
+import org.springframework.util.Assert;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * Simple JavaBean domain object representing an owner.
+ */
+@Entity
+@Table(name = "owners")
+public class Owner extends Person {
+
+	@Column
+	@NotBlank(message = "Please enter the address")
+	private String address;
+
+	@Column
+	@NotBlank(message = "Please enter the city")
+	private String city;
+
+	@Column
+	@NotBlank(message = "Please enter the telephone number")
+	@Pattern(regexp = "\\d{10}", message = "Telephone number must be 10 digits")
+	private String telephone;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "owner_id")
+	@OrderBy("name")
+	private final List<Pet> pets = new ArrayList<>();
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return this.telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public List<Pet> getPets() {
+		return this.pets;
+	}
+
+	public void addPet(Pet pet) {
+		if (pet.isNew()) {
+			getPets().add(pet);
+		}
+	}
+
+	public Pet getPet(String name) {
+		return getPet(name, false);
+	}
+
+	public Pet getPet(Integer id) {
+		for (Pet pet : getPets()) {
+			if (!pet.isNew()) {
+				Integer compId = pet.getId();
+				if (Objects.equals(compId, id)) {
+					return pet;
+				}
+			}
+		}
+		return null;
+	}
+
+	public Pet getPet(String name, boolean ignoreNew) {
+		for (Pet pet : getPets()) {
+			String compName = pet.getName();
+			if (compName != null && compName.equalsIgnoreCase(name)) {
+				if (!ignoreNew || !pet.isNew()) {
+					return pet;
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringCreator(this)
+			.append("id", this.getId())
+			.append("new", this.isNew())
+			.append("lastName", this.getLastName())
+			.append("firstName", this.getFirstName())
+			.append("address", this.address)
+			.append("city", this.city)
+			.append("telephone", this.telephone)
+			.toString();
+	}
+
+	public void addVisit(Integer petId, Visit visit) {
+
+		Assert.notNull(petId, "Pet identifier must not be null!");
+		Assert.notNull(visit, "Visit must not be null!");
+
+		Pet pet = getPet(petId);
+
+		Assert.notNull(pet, "Invalid Pet identifier!");
+
+		pet.addVisit(visit);
+	}
+}
